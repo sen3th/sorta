27b3012995f707b61bc2ca806973f6cfcda188da
Seneth <126297209+sen3th@users.noreply.github.com>
2026-04-30 14:07:49 -0400

    added videos, documents and zipped extensions to the file category helper

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def get_filecategory(file_path):
         return "images"
     if suffix in [".pdf"]:
         return "pdfs"
+    if suffix in ["mp4"]:
+        return "videos"
+    if suffix in [".txt", "docx"]:
+        return "documents"
+    if suffix in [".zip"]:
+        return "archives"
